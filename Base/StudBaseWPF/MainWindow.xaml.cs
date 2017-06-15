377a4f7622ecfe2eacd09fd988ac6c65a6d968e5
Resked <taratynov.99@mail.ru>
2017-06-15 14:00:18 +0700

    123

--- Base/StudBaseWPF/MainWindow.xaml.cs
+++ Base/StudBaseWPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace StudBaseWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
