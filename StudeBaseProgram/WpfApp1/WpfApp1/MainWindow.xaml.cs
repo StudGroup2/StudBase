3b796301d0935614f52d3762ced29f5e59de9806
Resked <taratynov.99@mail.ru>
2017-06-16 14:01:49 +0700

    123

--- StudeBaseProgram/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ StudeBaseProgram/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace StudBaseWPF
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -23,6 +23,26 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Тимин А.А. & Таратынов А.Н.", "Авторы");
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
